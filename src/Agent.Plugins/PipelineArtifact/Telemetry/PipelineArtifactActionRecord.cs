1d3ccef11d1a1f4db5c820fd1233080ddb8db2de
Nick Rodriguez <v-nicro@microsoft.com>
2019-05-31 16:40:17 -0700

    typo...

--- src/Agent.Plugins/PipelineArtifact/Telemetry/PipelineArtifactActionRecord.cs
+++ src/Agent.Plugins/PipelineArtifact/Telemetry/PipelineArtifactActionRecord.cs
@@ -21,7 +21,7 @@ namespace Agent.Plugins.PipelineArtifact.Telemetry
         protected override void SetMeasuredActionResult<T>(T value)
         {
             string valueType = value.GetType().ToString();
-            if (valueType == nameof(PublishResult)
+            if (valueType == nameof(PublishResult))
             {
                 PublishResult result = value as PublishResult;
                 FileCount = result.FileCount;
